9fa3919c347f652bce3402e49440637ff3bd467f
Zach Butler <zach.standley93@gmail.com>
2019-07-12 17:06:19 -0700

    wrote a test to create a penguin object using POST

---
+++ test/penguin.test.js
@@ -0,0 +1,19 @@
+const request = require('supertest');
+const app = require('../lib/app');
+
+// { type: 'emperor', name: 'lucy', age: '10' }
+// CRUD (create, read, update, delete)
+describe('penguin routes', () => {
+    it('can create a penguin with POST', () => {
+        return request(app)
+            .post('/api/v1/penguins')
+            .send({ type: 'emperor', name: 'lucy', age: 10 })
+            .then(res => {
+                expect(res.body).toEqual({
+                    type: 'emperor',
+                    name: 'lucy',
+                    age: 10
+                });
+            });
+    });
+});
